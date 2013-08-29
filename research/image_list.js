aba83911884ae09618f99abbf7f658f26faf01cd
FrankyYang <franky.z.yang@gmail.com>
2013-08-29 19:24:13 +0800

    Some pages created.

---
+++ research/image_list.js
@@ -0,0 +1,18 @@
+$(function () {
+    window.Action = {};
+    Action.detail = function (id) {
+        $('#DetailDialog').dialog({
+            title : '节目详情',
+            modal : true,
+            width : 800,
+            buttons : {
+                '关闭' : function() {
+                    $(this).dialog('close');
+                }
+            }
+        });
+
+        $('#PlayerContent').html('<iframe src="http://lego.iqiyi.com/video/preview?objectId=100009567&amp;type=0" class="custom-iframe" frameborder="0" scrolling="no"></iframe>');
+    };
+
+});
